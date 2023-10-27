a5765ccb8d1921b100227c4526a8ca1480ecddc8
Karanveer101 <kvsd008@gmail.com>
2023-10-26 21:46:34 -0400

    addedloader

--- client/src/components/Shared/DogDisplay.js
+++ client/src/components/Shared/DogDisplay.js
@@ -8,77 +8,95 @@ import { useNavigate } from "react-router-dom";
 import { homeDogDisplay } from "../../services/dogService";
 import { toggleFavorite } from "../../services/favoritedService";
 import { useAuth } from "../../Context/AuthContext";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function DogDisplay(props) {
     const { favoriteDogs, setFavoriteDogs } = props;
     const { userRole } = useAuth();
     const [dogs, setDogs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     console.log(dogs);
     const navigate = useNavigate();
 
     //display dogs for home page
     useEffect(() => {
-        homeDogDisplay(setDogs);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        const fetchData = async () => {
+            await homeDogDisplay(setDogs);
+            setIsLoading(false);
+        };
+
+        fetchData();
     }, []);
 
     return (
         <section className='dogDisplayContainer'>
             <h1>Adoptable Dogs</h1>
-            <div className='dogGrid'>
-                {dogs.map((dog) => (
-                    <div
-                        key={dog.id}
-                        className='dog'
-                        onClick={() =>
-                            navigate("/dog-profile", { state: { dog } })
-                        }
-                    >
-                        {userRole === "admin" ? (
-                            ""
-                        ) : (
-                            <div
-                                className='favorite'
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    toggleFavorite(
-                                        dog,
-                                        dog.id,
-                                        favoriteDogs,
-                                        setFavoriteDogs,
-                                        navigate
-                                    );
-                                }}
-                            >
-                                {favoriteDogs.includes(dog.id) ? (
-                                    <IconContext.Provider
-                                        value={{
-                                            className: "favoriteSuccessIcon",
-                                            size: 25,
-                                        }}
-                                    >
-                                        <AiFillHeart />
-                                    </IconContext.Provider>
-                                ) : (
-                                    <IconContext.Provider
-                                        value={{
-                                            className: "favoriteIcon",
-                                            size: 25,
-                                        }}
-                                    >
-                                        <RiHeartAddFill />
-                                    </IconContext.Provider>
-                                )}
-                            </div>
-                        )}
-                        <img src={dog.photos[0].medium} alt='dogImage' />
-                        <h2>{dog.name}</h2>
-                        <p>
-                            {dog.breeds.primary} | {dog.gender}
-                        </p>
-                    </div>
-                ))}
-            </div>
+            {isLoading ? (
+                <div>
+                    <ClipLoader
+                        size={50}
+                        aria-label='Loading Spinner'
+                        data-testid='loader'
+                        color='#217693'
+                    />
+                </div>
+            ) : (
+                <div className='dogGrid'>
+                    {dogs.map((dog) => (
+                        <div
+                            key={dog.id}
+                            className='dog'
+                            onClick={() =>
+                                navigate("/dog-profile", { state: { dog } })
+                            }
+                        >
+                            {userRole === "admin" ? (
+                                ""
+                            ) : (
+                                <div
+                                    className='favorite'
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        toggleFavorite(
+                                            dog,
+                                            dog.id,
+                                            favoriteDogs,
+                                            setFavoriteDogs,
+                                            navigate
+                                        );
+                                    }}
+                                >
+                                    {favoriteDogs.includes(dog.id) ? (
+                                        <IconContext.Provider
+                                            value={{
+                                                className:
+                                                    "favoriteSuccessIcon",
+                                                size: 25,
+                                            }}
+                                        >
+                                            <AiFillHeart />
+                                        </IconContext.Provider>
+                                    ) : (
+                                        <IconContext.Provider
+                                            value={{
+                                                className: "favoriteIcon",
+                                                size: 25,
+                                            }}
+                                        >
+                                            <RiHeartAddFill />
+                                        </IconContext.Provider>
+                                    )}
+                                </div>
+                            )}
+                            <img src={dog.photos[0].medium} alt='dogImage' />
+                            <h2>{dog.name}</h2>
+                            <p>
+                                {dog.breeds.primary} | {dog.gender}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </section>
     );
 }
